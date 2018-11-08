b021f0674522e708fbacbc2097285a7ea20f23e2
trungnx <trung@kyber.network>
2018-11-08 13:24:11 +0700

    remove unused package

--- mvp/client-mvp/app/components/Header.js
+++ mvp/client-mvp/app/components/Header.js
@@ -1,11 +1,10 @@
 import Link from 'next/link';
-import logo from '../assets/images/logo.svg';
 
 const Header = () => (
   <div className={"home-header"}>
     <div className={"home-logo"}>
       <Link href="/">
-        <img src={logo} alt="StackChain" className={"home-logo-img"}/>
+        <img src="/static/logo.svg" alt="StackChain" className={"home-logo-img"}/>
       </Link>
     </div>
     <div className={"sign-in"}>
